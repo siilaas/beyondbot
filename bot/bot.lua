b6b0ef6e8c688b7d73be9e287ca06be0661bd44f
siilaas <fire_matter@yahoo.com>
2017-09-11 02:03:47 +0430

    Add files via upload

--- bot/bot.lua
+++ bot/bot.lua
@@ -84,20 +84,36 @@ function create_config( )
     "plugins",
     "tools"
  },
-    sudo_users = {108363478},
+    sudo_users = {157059515},
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
-    info_text = [[》silas v1
+    info_text = [[》Beyond Reborn v1
 An advanced administration bot based on https://valtman.name/telegram-cli
 
-》https://github.com/oralius 
+》https://github.com/BeyondTeam/BDReborn 
 
 》Admins :
-》@oralius ➣ Founder & Developer《
+》@SoLiD ➣ Founder & Developer《
+》@Makan ➣ Developer《
+》@Rixel ➣ Developer 《
+》@Exacute ➣ Developer《
+》@CiveY ➣ Developer
+》@ArmanDev ➣ Manager《
+》@MrPars ➣ Manager《
+
+》Special thanks to :
+》@Vysheng
+》@MrHalix
+》@K_a_I_i_I_i_n_u_x
+》@Nero_Dev
+》And Beyond Team Members
+
+》Our channel :
+》@BeyondTeam《
 
 》Our website :
-》http://lua.ir
+》http://BeyondTeam.ir
 ]],
   }
   serialize_to_file(config, './data/config.lua')
